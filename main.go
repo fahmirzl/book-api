c16fa62d2351c376cebad77d234d7092aa5b8969
fahmirzl <fahmirizal.dev@gmail.com>
2025-06-14 21:17:22 +0700

    (feat): Get a port form .ENV

--- main.go
+++ main.go
@@ -3,11 +3,12 @@ package main
 import (
 	"book-api/db"
 	"book-api/routers"
+	"os"
 )
 
 func main() {
 	db.Init()
 	defer db.DB.Close()
 
-	routers.StartServer().Run(":8080")
+	routers.StartServer().Run(":" + os.Getenv("PORT"))
 }
